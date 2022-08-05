34d50ef3f627777d75ed38c5c093910cd8522e93
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-08-05 10:15:22 +0600

    feat: home page complete

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
+import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../footer/Footer";
 
 const Home = () => {
 	return (
@@ -9,7 +14,13 @@ const Home = () => {
 			<Navbar />
 			<Header />
 			<div className="homeContainer">
-				
+				<Featured />
+				<h1 className="homeTitle">Browse by property type</h1>
+				<PropertyList />
+				<h1 className="homeTitle">Home guest love</h1>
+				<FeaturedProperties />
+				<MailList />
+				<Footer />
 			</div>
 		</div>
 	);
